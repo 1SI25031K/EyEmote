0c9ca8e3f11bac10e3eaa23fce27d46aae7fe3e3
Kosei M <miyamoto.kosei.698@s.kyushu-u.ac.jp>
2026-01-21 13:09:05 +0900

    Added FluidSoulView and State for available on iOS17 and later.

---
+++ FluidSoulView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  EyEmote
+//
+//  Created by Kosei Miyamoto on 2026/01/21.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
